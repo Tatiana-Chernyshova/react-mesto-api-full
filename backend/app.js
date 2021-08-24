237166ef4b4cc9b2e7be713d0fcd50367ac9fa65
Vladimir Chernyshov <Tram27mrld@yandex.ru>
2021-08-24 12:32:42 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const cors = require('cors');
 const rootRouter = require('./routes/index');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -14,6 +15,15 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
+
+const corsOptions = {
+  origin: [
+    'https://mrld.nomoredomains.rocks/',
+    'http://mrld.nomoredomains.rocks/',
+    'localhost:3000',
+  ],
+  // credentials: true,
+};
 
 require('dotenv').config();
 
@@ -28,6 +38,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 app.use(limiter);
+app.use(cors(corsOptions));
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
